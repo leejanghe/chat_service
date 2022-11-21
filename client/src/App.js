9f3c6894aaaf8a688cb94d77cbe6674cb33595da
leejanghe <babybye777@gmail.com>
2022-11-21 23:16:05 +0900

    env 설정 다시, 회원가입 페이지 진행

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,11 @@ import LoginPage from "./pages/LoginPage/LoginPage";
 import RegisterPage from "./pages/RegisterPage/RegisterPage";
 import ChatPage from "./pages/ChatPage/ChatPage";
 
+// import firebase from "firebase/compat/app";
+// import "firebase/compat/auth";
+// import "firebase/compat/firestore";
+// import "firebase/compat/storage";
+// console.log("firebase", firebase);
 function App() {
   return (
     <Router>
--- client/src/firebase.js
+++ client/src/firebase.js
@@ -1,19 +1,38 @@
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+// import { initializeApp } from "firebase/app";
+// import firebase from "firebase/app";
 import "firebase/auth";
 import "firebase/database";
 import "firebase/storage";
 
+import firebase from "firebase/compat/app";
 const firebaseConfig = {
-  apiKey: REACT_APP_API_KEY,
-  authDomain: REACT_APP_AUTH_DOMAIN,
-  projectId: REACT_APP_PROJECT_ID,
-  storageBucket: REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: REACT_APP_MESSAGING_SENDER_ID,
-  appId: REACT_APP_APP_ID,
-  measurementId: REACT_APP_MEASUREMENT_ID,
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
 };
-
+console.log("firebaseConfig", firebaseConfig);
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+firebase.initializeApp(firebaseConfig);
 //   const analytics = getAnalytics(app);
+
+export default firebase;
+// export const authService = firebase.auth();
+// apiKey: process.env.REACT_APP_API_KEY,
+// authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+// projectId: process.env.REACT_APP_PROJECT_ID,
+// storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+// messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+// appId: process.env.REACT_APP_APP_ID,
+// measurementId: process.env.REACT_APP_MEASUREMENT_ID,
+
+// apiKey: "AIzaSyAoQb5me6-VubVippyDwobjt6eQazxC0Mg",
+// authDomain: "react-firebase-chat-e3c29.firebaseapp.com",
+// projectId: "react-firebase-chat-e3c29",
+// storageBucket: "react-firebase-chat-e3c29.appspot.com",
+// messagingSenderId: "427711982392",
+// appId: "1:427711982392:web:1bb42e5cf0e98e6c77afcd",
+// measurementId: "G-T4XPN50K07",
