fde28e1e2b54c2ad935f9b9d3aefb051000d4b9a
leejanghe <babybye777@gmail.com>
2022-11-23 02:19:35 +0900

    패널 추가 및 그리드 작업

---
+++ client/src/pages/ChatPage/MainPanel/MainPanel.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function MainPanel(props) {
+  return <div>MainPanel</div>;
+}
+
+export default MainPanel;
---
+++ client/src/pages/ChatPage/SidePanel/SidePanel.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function SidePanel(props) {
+  return <div>sidepanel</div>;
+}
+
+export default SidePanel;
