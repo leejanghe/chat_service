2e0ae5f6cf2a5e7fbd726b4539e994f75a0a2167
leejanghe <babybye777@gmail.com>
2022-11-23 01:07:02 +0900

    router처리

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,36 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Route, Routes, useNavigate } from "react-router-dom";
 import LoginPage from "./pages/LoginPage/LoginPage";
 import RegisterPage from "./pages/RegisterPage/RegisterPage";
 import ChatPage from "./pages/ChatPage/ChatPage";
-
+import { useEffect } from "react";
+import { authService } from "./firebase";
+// import { useNavigate } from "react-router-dom";
 // import firebase from "firebase/compat/app";
 // import "firebase/compat/auth";
 // import "firebase/compat/firestore";
 // import "firebase/compat/storage";
 // console.log("firebase", firebase);
 function App() {
+  const navigate = useNavigate();
+  useEffect(() => {
+    authService.onAuthStateChanged((user) => {
+      if (user) {
+        console.log("user", user);
+        //navigate
+
+        navigate("/chat");
+      } else {
+        navigate("/login");
+      }
+    });
+  }, []);
+
   return (
-    <Router>
-      <Routes>
-        <Route exact path="/" element={<ChatPage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/register" element={<RegisterPage />} />
-      </Routes>
-    </Router>
+    <Routes>
+      <Route exact path="/" element={<ChatPage />} />
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/register" element={<RegisterPage />} />
+    </Routes>
   );
 }
 
--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-
+import { BrowserRouter as Router } from "react-router-dom";
 import { Provider } from "react-redux";
 
 import { applyMiddleware, createStore } from "redux";
@@ -26,7 +26,9 @@ root.render(
           window.__REDUX_DEVTOOLS_EXTENSION__()
       )}
     >
-      <App />
+      <Router>
+        <App />
+      </Router>
     </Provider>
   </React.StrictMode>
 );
