d6fdb16b4beb607cecdeeb2345f924095fe4a549
leejanghe <babybye777@gmail.com>
2022-11-25 00:28:57 +0900

    file upload

--- client/src/pages/ChatPage/SidePanel/UserPanel.js
+++ client/src/pages/ChatPage/SidePanel/UserPanel.js
@@ -3,12 +3,33 @@ import { IoIosChatboxes } from "react-icons/io";
 import Dropdown from "react-bootstrap/Dropdown";
 import Image from "react-bootstrap/Image";
 import { useSelector } from "react-redux";
-import { authService } from "../../../firebase";
+import { authService, storageService } from "../../../firebase";
+import { useRef } from "react";
+
 function UserPanel(props) {
   const user = useSelector((state) => state.user.currentUser);
-
+  const inputOpenImageRef = useRef();
   const handleLogout = () => {
     authService.signOut();
+  };
+
+  const handleOpenImageRef = () => {
+    inputOpenImageRef.current.click();
+  };
+
+  const handleUploadImage = async (event) => {
+    const file = event.target.files[0];
+    const metadata = { contentType: file.type };
+    try {
+      // 스토리지에 파일 저장
+      let uploadTaskSnapshot = await storageService
+        .ref()
+        .child(`user_image/${user.uid}`)
+        .put(file, metadata);
+      console.log(uploadTaskSnapshot);
+    } catch (error) {
+      // alert(error);
+    }
   };
 
   return (
@@ -24,16 +45,27 @@ function UserPanel(props) {
           style={{ width: "30px", height: "30px", marginTop: "3px" }}
           roundedCircle
         />
+
+        <input
+          type={"file"}
+          onChange={handleUploadImage}
+          style={{ display: "none" }}
+          accept="image/jpeg, image/png"
+          ref={inputOpenImageRef}
+        />
+
         <Dropdown>
           <Dropdown.Toggle
             style={{ background: "transparent", boder: "0px" }}
             id="dropdown-basic"
           >
-            {user.displayName}
+            {user && user.displayName}
           </Dropdown.Toggle>
 
           <Dropdown.Menu>
-            <Dropdown.Item href="#/action-1">프로필 사진 변경</Dropdown.Item>
+            <Dropdown.Item onClick={handleOpenImageRef}>
+              프로필 사진 변경
+            </Dropdown.Item>
             <Dropdown.Item onClick={handleLogout}>로그아웃</Dropdown.Item>
           </Dropdown.Menu>
         </Dropdown>
